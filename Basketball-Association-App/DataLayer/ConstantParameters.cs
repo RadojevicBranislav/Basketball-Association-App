76799ae9e08da2d66cbd471e5f4bad0c65737321
Bane <branislavradojevicit4@gmail.com>
2023-01-14 21:03:26 +0100

    Added CRUD functionalities to KlubRepository

---
+++ Basketball-Association-App/DataLayer/ConstantParameters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public static class ConstantParameters
+    {
+
+        public static string connString = "Data Source=(localdb)\\ProjectModels;Initial Catalog=BasketballAssociation;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+
+    }
+}
--- Basketball-Association-App/DataLayer/KlubRepository.cs
+++ Basketball-Association-App/DataLayer/KlubRepository.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DataLayer.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,92 @@ namespace DataLayer
 {
     public class KlubRepository
     {
+
+        public List<Klub> GetKlubs()
+        {
+
+            using (SqlConnection conn = new SqlConnection(ConstantParameters.connString))
+            {
+                
+                SqlCommand sql = new SqlCommand();
+
+                sql.Connection = conn;
+                
+                sql.CommandText = ("SELECT * FROM Klubovi");
+
+                List<Klub> ls = new List<Klub>();
+
+                conn.Open();
+
+                SqlDataReader dataReader = sql.ExecuteReader();
+
+                while(dataReader.Read())
+                {
+                    Klub k = new Klub();
+
+                    k.Id = dataReader.GetInt32(0);
+
+                    k.Naziv = dataReader.GetString(1);
+
+                    k.Lokacija = dataReader.GetString(2);
+
+                    ls.Add(k);
+
+                }
+
+                return ls;
+            }
+
+        }
+
+        public int InsertKlub(Klub klub)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(ConstantParameters.connString))
+            {
+                SqlCommand sql = new SqlCommand();
+
+                sql.Connection = sqlConnection;
+
+                sql.CommandText = string.Format("INSERT INTO Klubovi VALUES('{0}',{1},)", klub.Naziv, klub.Lokacija);
+
+                sqlConnection.Open();
+
+                return sql.ExecuteNonQuery();
+            }
+        }
+
+        public int Deleteklub(int id)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(ConstantParameters.connString))
+            {
+                SqlCommand sql = new SqlCommand();
+
+                sql.Connection = sqlConnection;
+
+                sql.CommandText = string.Format("DELETE FROM Klubovi WHERE Id={0}", id);
+
+                sqlConnection.Open();
+
+                return sql.ExecuteNonQuery();
+            }
+        }
+
+        public int UpdateKlub(int id, string newName)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(ConstantParameters.connString))
+            {
+                SqlCommand sql = new SqlCommand();
+
+                sql.Connection = sqlConnection;
+
+                sql.CommandText = string.Format("UPDATE Klubovi SET naziv={1} WHERE Id={0}", id, newName);
+
+                sqlConnection.Open();
+
+                return sql.ExecuteNonQuery();
+            }
+        }
     }
+
+    
 }
