fa7a870cf5945d09d2345c9c66a8d03cdb451174
Branislav Radojevic <branislavradojevicit4@gmail.com>
2023-02-14 00:42:53 +0100

    Created 2 business classes, left collectiveBusiness

--- Basketball-Association-App/BusinessLayer/KosarkasBusiness.cs
+++ Basketball-Association-App/BusinessLayer/KosarkasBusiness.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataLayer;
+using DataLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +10,27 @@ namespace BusinessLayer
 {
     public class KosarkasBusiness
     {
+        private KosarkasRepository kosarkasRepository = new KosarkasRepository();
 
+        public List<Kosarkas> GetKosarkasi()
+        {
+            return kosarkasRepository.GetKosarkasi();
+        }
+
+        public bool insertKosarkas(Kosarkas kosarkas)
+        {
+            return kosarkasRepository.InsertKosarkas(kosarkas) > 0;
+        }
+
+        public bool deleteKosarkas(int kosarkasId)
+        {
+            return kosarkasRepository.DeleteKosarkas(kosarkasId) > 0;
+        }
+
+        public Kosarkas getKosarkasById(int kosarkasId)
+        {
+            return kosarkasRepository.GetKosarkasById(kosarkasId);
+        }
 
     }
 }
--- Basketball-Association-App/BusinessLayer/UgovorBusiness.cs
+++ Basketball-Association-App/BusinessLayer/UgovorBusiness.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataLayer;
+using DataLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,16 @@ namespace BusinessLayer
 {
     public class UgovorBusiness
     {
+        private UgovorRepository ugovorRepository = new UgovorRepository();
+
+        public List<Ugovor> getUgovori()
+        {
+            return ugovorRepository.GetUgovors();
+        }
+
+        public bool insertUgovor(Ugovor ugovor)
+        {
+            return ugovorRepository.InsertUgovor(ugovor) > 0;
+        }
     }
 }
